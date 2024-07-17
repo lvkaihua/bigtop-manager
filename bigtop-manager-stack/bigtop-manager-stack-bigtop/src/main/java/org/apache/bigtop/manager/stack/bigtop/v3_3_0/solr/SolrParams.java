768a56b7f8a32e720f7be6392dd01f5d158eb957
lvkaihua <215957054@qq.com>
2024-07-17 18:10:17 +0800

    add solr

---
+++ bigtop-manager-stack/bigtop-manager-stack-bigtop/src/main/java/org/apache/bigtop/manager/stack/bigtop/v3_3_0/solr/SolrParams.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.bigtop.manager.stack.bigtop.v3_3_0.solr;
+
+import lombok.Getter;
+import org.apache.bigtop.manager.common.message.entity.payload.CommandPayload;
+import org.apache.bigtop.manager.stack.common.annotations.GlobalParams;
+import org.apache.bigtop.manager.stack.common.utils.BaseParams;
+import org.apache.bigtop.manager.stack.common.utils.LocalSettings;
+
+import java.util.Map;
+
+@Getter
+public class SolrParams extends BaseParams {
+
+    private String solrLogDir = "/var/log/solr";
+    private String solrPidDir = "/var/run/solr";
+    private String solrDataDir = "/var/lib/solr/data";
+    private String solrPidFile = solrPidDir + "/solr_server.pid";
+
+    public SolrParams(CommandPayload commandPayload) {
+        super(commandPayload);
+        globalParamsMap.put("java_home", "/usr/local/java");
+        globalParamsMap.put("solr_home", serviceHome());
+        globalParamsMap.put("security_enabled", false);
+        globalParamsMap.put("solr_pid_file", solrPidFile);
+        globalParamsMap.put("yarn_user", user());
+        globalParamsMap.put("yarn_group", group());
+    }
+
+    @GlobalParams
+    public Map<String, Object> solrxml() {
+        return LocalSettings.configurations(serviceName(), "solr-xml");
+    }
+    @GlobalParams
+    public Map<String, Object> solrLog4j() {
+        return LocalSettings.configurations(serviceName(), "solr-log4j");
+    }
+
+    @GlobalParams
+    public Map<String, Object> solrEnv() {
+        Map<String, Object> solrEnv = LocalSettings.configurations(serviceName(), "solr-env");
+        solrLogDir = (String) solrEnv.get("solr_log_dir");
+        solrPidDir = (String) solrEnv.get("solr_pid_dir");
+        solrDataDir = (String) solrEnv.get("solr_datadir");
+        solrPidFile = solrPidDir + "/solr_server.pid";
+        return solrEnv;
+    }
+}
---
+++ bigtop-manager-stack/bigtop-manager-stack-bigtop/src/main/java/org/apache/bigtop/manager/stack/bigtop/v3_3_0/solr/SolrSetup.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.bigtop.manager.stack.bigtop.v3_3_0.solr;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.bigtop.manager.common.constants.Constants;
+import org.apache.bigtop.manager.common.shell.ShellResult;
+import org.apache.bigtop.manager.spi.stack.Params;
+import org.apache.bigtop.manager.stack.common.utils.linux.LinuxFileUtils;
+import java.text.MessageFormat;
+import java.util.Map;
+import static org.apache.bigtop.manager.common.constants.Constants.PERMISSION_755;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SolrSetup {
+
+    public static ShellResult config(Params params) {
+        log.info("solrSetup config");
+        SolrParams solrParams = (SolrParams) params;
+
+        String confDir = solrParams.confDir();
+        String solrUser = solrParams.user();
+        String solrGroup = solrParams.group();
+        Map<String, Object> solrEnv = solrParams.solrEnv();
+
+        LinuxFileUtils.createDirectories(
+                solrParams.getSolrDataDir(), solrUser, solrGroup, PERMISSION_755, true);
+        LinuxFileUtils.createDirectories(
+                solrParams.getSolrLogDir(), solrUser, solrGroup, PERMISSION_755, true);
+        LinuxFileUtils.createDirectories(
+                solrParams.getSolrPidDir(), solrUser, solrGroup, PERMISSION_755, true);
+
+
+        // solr-env.xml
+        LinuxFileUtils.toFileByTemplate(
+                solrEnv.get("content").toString(),
+                MessageFormat.format("{0}/solr-env.xml", confDir),
+                solrUser,
+                solrGroup,
+                Constants.PERMISSION_755,
+                solrParams.getGlobalParamsMap());
+        // solr-log4j.xml
+        LinuxFileUtils.toFileByTemplate(
+                solrParams.solrLog4j().get("content").toString(),
+                MessageFormat.format("{0}/solr-log4j.xml", confDir),
+                solrUser,
+                solrGroup,
+                Constants.PERMISSION_755,
+                solrParams.getGlobalParamsMap());
+        // solr-xml.xml
+        LinuxFileUtils.toFileByTemplate(
+                solrParams.solrxml().get("content").toString(),
+                MessageFormat.format("{0}/solr-xml.xml", confDir),
+                solrUser,
+                solrGroup,
+                Constants.PERMISSION_755,
+                solrParams.getGlobalParamsMap());
+
+        return ShellResult.success("solr Server Configure success!");
+    }
+}
